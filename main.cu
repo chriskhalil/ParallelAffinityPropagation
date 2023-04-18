8259da4a4c7b9534205619ca188d9ed3bc50cba0
Deltanoid <alimoh.chehab@gmail.com>
2023-04-18 16:28:50 +0300

    Added Files
    Added CUDA files to start parallelization on.

---
+++ main.cu
@@ -0,0 +1,20 @@
+#include "AffinityPropagation.h"
+
+int main(int argc, char**argv) {
+
+    cudaDeviceSynchronize();
+
+    // Allocate memory and initialize data
+
+    // Copy data to GPU
+
+    // Compute on GPU
+
+    // Copy data from GPU
+
+    // Free GPU memory
+
+    // Verify result
+
+    // Free memory
+}
